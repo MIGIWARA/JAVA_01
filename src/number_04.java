6fa5d5a00dd847eeef3359929bb747cc0122e25f
MIGIWARA <99168445+MIGIWARA@users.noreply.github.com>
2022-05-06 10:43:06 +0300

    homework

--- src/number_04.java
+++ src/number_04.java
@@ -1,7 +1,7 @@
 public class number_04 {
     public static int getMaxInt(){
         int a;
-        int max = Integer.getMaxInt(a);
+        int max = Integer.max(a);
         return max;
     }
 }
