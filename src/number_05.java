76dcc3964d67ee3e4e48f9ed99b99b02f212e17f
MIGIWARA <99168445+MIGIWARA@users.noreply.github.com>
2022-05-08 12:48:54 +0300

    homework

---
+++ src/number_05.java
@@ -0,0 +1,15 @@
+public class number_05 {
+    // метод main чисто для меня
+    public static void main(String[] args) {
+       int result = incrementIfPositive(-5);
+        System.out.println(result);
+    }
+    public static int incrementIfPositive(int a) {
+        if(a < 0){
+            return a;
+        }else {
+            a++;
+        }
+        return a;
+    }
+}
