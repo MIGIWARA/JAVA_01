b8b640bb57fa8d07f8aa9da01299126d23966b00
MIGIWARA <99168445+MIGIWARA@users.noreply.github.com>
2022-05-08 13:25:07 +0300

    homework

---
+++ src/number_06.java
@@ -0,0 +1,11 @@
+public class number_06 {
+    public static void main(String[] args) {
+       printJavaWord(6);
+    }
+    public static void printJavaWord(int count) {
+        for (int i = 0; i < count ; i++) {
+            System.out.println("Java");
+        }
+    }
+
+}
