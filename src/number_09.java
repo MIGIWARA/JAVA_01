9978525991215fd689a19f4df96bc37aaefe0a5b
MIGIWARA <99168445+MIGIWARA@users.noreply.github.com>
2022-05-09 12:20:17 +0300

    homework

---
+++ src/number_09.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class number_09 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Как вас зовут?");
+        String username = in.nextLine();
+        System.out.println("Здравствуйте, " + username);
+
+    }
+}
