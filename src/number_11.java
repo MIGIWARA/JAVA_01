c6eb034f11e8db25fbbe045fd7b4acf68c9fff21
MIGIWARA <99168445+MIGIWARA@users.noreply.github.com>
2022-05-10 15:44:41 +0300

    homework

---
+++ src/number_11.java
@@ -0,0 +1,24 @@
+/**
+ * Урок с кодом
+ * Cоздайте метод static int[] fillArray(int x), который принимает целое
+ * число и возвращает массив целых чисел, размером равный этому числу и
+ * заполненный числами от 0 до числа, меньшего на единицу, чем принятое.
+ *
+ * Пример ввода параметра: 3
+ * Пример возвращаемого массива: [0,1,2]
+ */
+public class number_11 {
+    public static void main(String[] args) {
+        fillArray(9);
+    }
+    static int[] fillArray(int x){
+        int[] values = new int [x];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = i + 1;// крч я писал i++ и он так не работал
+            System.out.print(values[i]+" ");
+
+        }
+        return values;
+    }
+
+}
